6ff435ee77d03911302f48c75ed51f9fb8c074f1
Azarias Boutin <azarias.boutin@gmail.com>
2018-11-27 09:33:42 +0100

    Showing the selected buttons

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -7,38 +7,54 @@ function id(id: string){
 export class UIUtils {
 
     private panels: HTMLElement[] = [];
+    private btns: HTMLButtonElement[] = [];
 
     constructor(private game: Game){
     }
 
     private hide = (el: HTMLElement) => el.classList.add('hiding');
     private show = (el: HTMLElement) => el.classList.remove('hiding');
+    private select = (el: HTMLElement) => el.classList.add('selected');
+    private deselect = (el: HTMLElement) => el.classList.remove('selected');
 
-    private togglePannel(panel: HTMLElement){
+
+    private togglePannel(panel: HTMLElement, btn: HTMLButtonElement){
         if(panel.classList.contains('pannel-2')){
             if(this.panels.length === 0){
                 this.panels.push(panel);
+                this.btns.push(btn);
             } else if(this.panels[0] === panel){
                 this.panels.map(x => this.hide(x));
+                this.btns.map(x => this.deselect(x));
+                this.btns = [];
                 this.panels = [];
                 return;
             } else {
                 this.panels.map(x => this.hide(x));
+                this.btns.map(x => this.deselect(x));
+                this.btns = [btn];
                 this.panels = [panel];
             }
         } else if(panel.classList.contains('pannel-3')){
             if(this.panels.length === 1){
+                this.btns.push(btn);
                 this.panels.push(panel);
             } else if(this.panels[1] === panel){
                 this.panels.pop();
+                this.btns.pop();
+                this.deselect(btn);
                 this.hide(panel);
                 return;
             } else {
                 this.hide(this.panels[1]);
+                this.deselect(this.btns[1]);
+                this.btns.pop();
                 this.panels.pop();
                 this.panels.push(panel);
+                this.btns.push(btn);
             }
         }
+        this.select(btn);
         this.show(panel);
     }
 
@@ -47,7 +63,7 @@ export class UIUtils {
         buttons.filter(x => x.hasAttribute('data-target')).map(x => {
             const target = x.getAttribute('data-target');
             x.addEventListener('click', () => {
-                this.togglePannel(id(target));
+                this.togglePannel(id(target), x);
             });
         });
     }
